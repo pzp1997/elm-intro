d4e58161f8cf93d0281c2897bcb5f31499372f28
Palmer Paul <pzpaul2002@yahoo.com>
2017-03-09 00:10:47 -0500

    add submission button to form

--- form.elm
+++ form.elm
@@ -1,8 +1,10 @@
 module Main exposing (..)
 
+import Char exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
+import String exposing (..)
 
 
 main =
@@ -17,12 +19,13 @@ type alias Model =
     { name : String
     , password : String
     , passwordAgain : String
+    , passwordMessage : (String, String)
     }
 
 
 model : Model
 model =
-    Model "" "" ""
+    Model "" "" "" ("", "")
 
 
 
@@ -33,6 +36,7 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Submit
 
 
 update : Msg -> Model -> Model
@@ -47,6 +51,9 @@ update msg model =
         PasswordAgain passwordAgain ->
             { model | passwordAgain = passwordAgain }
 
+        Submit ->
+            { model | passwordMessage = viewValidation model }
+
 
 
 -- VIEW
@@ -54,21 +61,27 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
+    let ( color, message ) = model.passwordMessage in
     div []
         [ input [ type_ "text", placeholder "Name", onInput Name ] []
         , input [ type_ "password", placeholder "Password", onInput Password ] []
         , input [ type_ "password", placeholder "Repeat password", onInput PasswordAgain ] []
-        , viewValidation model
+        , button [ onClick Submit ] [ text "Submit" ]
+        , div [ style [ ( "color", color ) ] ] [ text message ]
         ]
 
 
-viewValidation : Model -> Html Msg
+viewValidation : Model -> (String, String)
 viewValidation model =
-    let
-        ( color, message ) =
-            if model.password == model.passwordAgain then
-                ( "green", "OK!" )
-            else
-                ( "red", "Passwords don't match!" )
-    in
-        div [ style [ ( "color", color ) ] ] [ text message ]
+    if String.length model.password < 8 then
+        ( "red", "Password too short!" )
+    else if not (String.any Char.isUpper model.password) then
+        ( "red", "Password must contain an uppercase letter!" )
+    else if not (String.any Char.isLower model.password) then
+        ( "red", "Password must contain a lowercase letter!" )
+    else if not (String.any Char.isDigit model.password) then
+        ( "red", "Password must contain a number!" )
+    else if model.password /= model.passwordAgain then
+        ( "red", "Passwords don't match!" )
+    else
+        ( "green", "OK!" )
